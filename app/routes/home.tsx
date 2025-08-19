b260cc60af364ca861a62a5eb17c9ea3410e510b
Sam Gaus <sam@gaus.co.uk>
2025-08-19 16:47:34 +0100

    Use correct event

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -120,7 +120,7 @@ export default function Home({
             type="checkbox"
             className="setting"
             checked={hideCompleted}
-            onInput={(e) => setHideCompleted(e.currentTarget.checked)}
+            onChange={(e) => setHideCompleted(e.currentTarget.checked)}
           />
           Hide fully donated monsters
         </label>
