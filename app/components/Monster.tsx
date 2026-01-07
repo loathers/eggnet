36487163d684209a309939857195ee5ad6950d4a
Sam Gaus <sam@gaus.co.uk>
2026-01-07 12:53:44 +0000

    Remove unused react import

--- app/components/Monster.tsx
+++ app/components/Monster.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useFetcher } from "react-router";
 import { clsx } from "clsx";
 import { decodeHTML } from "entities";
